7980e4403a0c39ce1e9cf94c4b2c1549ee65ef74
BogdanArsic <ab190329d@student.etf.bg.ac.rs>
2022-05-16 23:18:47 +0200

    Dodao sam v2 osnove implementacije projekta

---
+++ Faza5/Implementacija/app/Models/bogdan/SviTagovi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\bogdan;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use function GuzzleHttp\Promise\all;
+
+class SviTagovi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tag';
+    protected $primaryKey = 'IDTag';
+
+    protected $fillable = [
+        'Name',
+    ];
+
+}
--- Faza5/Implementacija/routes/web.php
+++ Faza5/Implementacija/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',[App\Http\Controllers\bogdan\GostController::class,'login'] )->name('login');
+
+
