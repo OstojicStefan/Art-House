5fe1f87cdbdec9b25128d8edc5416a664e4d1453
BogdanArsic <ab190329d@student.etf.bg.ac.rs>
2022-05-24 00:59:46 +0200

    Izmenio sam greske koje su nastale spajanjem grana

--- Faza5/Implementacija/routes/web.php
+++ Faza5/Implementacija/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\stefan\CreateExhibitionController;
+use App\Http\Controllers\bogdan\GostController as GostControllerBogdan;
+use App\Http\Controllers\bogdan\RegistrovaniKontroler as RegistrovaniKontrolerBogdan;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,28 +21,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login',[App\Http\Controllers\bogdan\GostController::class,'login'] )->name('login');
-Route::post('/loginSubmit',[App\Http\Controllers\bogdan\GostController::class,'loginSubmit'] )->name('loginSubmit');
-Route::get('/register',[App\Http\Controllers\bogdan\GostController::class,'register'] )->name('register');
-Route::post('/registerSubmit',[App\Http\Controllers\bogdan\GostController::class,'registerSubmit'] )->name('registerSubmit');
-Route::get('/addTags',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'addTags'] )->name('addTags');
-Route::get('/removeTags',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'removeTags'] )->name('removeTags');
-Route::get('/createAuctionVirtual',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'createAuctionVirtual'] )->name('createAuctionVirtual');
-Route::post('/createAuctionSubmit',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'createAuctionSubmit'] )->name('createAuctionSubmit');
-Route::get('/createAuctionPhysical',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'createAuctionPhysical'] )->name('createAuctionPhysical');
-Route::post('/removeTags2',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'removeTags2'] )->name('removeTags2');
-Route::post('/addTagsSubmit',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'addTagsSubmit'] )->name('addTagsSubmit');
-Route::get('/upgradeUserRoles',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'upgradeUserRoles'] )->name('upgradeUserRoles');
-Route::post('/upgradeUserRolesSubmit',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'upgradeUserRolesSubmit'] )->name('upgradeUserRolesSubmit');
-Route::get('/logout',[App\Http\Controllers\bogdan\RegistrovaniKontroler::class,'logout'] )->name('logout');
-Route::get('/test2',[App\Http\Controllers\bogdan\GostController::class,'test2'] )->name('test2');
-Route::get('/test3',[App\Http\Controllers\bogdan\GostController::class,'test3'] )->name('test3');
-Route::get('/test4',[App\Http\Controllers\bogdan\GostController::class,'test4'] )->name('test4');
-
+Route::get('/login',[GostControllerBogdan::class,'login'] )->name('login');
+Route::post('/loginSubmit',[GostControllerBogdan::class,'loginSubmit'] )->name('loginSubmit');
+Route::get('/register',[GostControllerBogdan::class,'register'] )->name('register');
+Route::post('/registerSubmit',[bogdan\GostControllerBogdan::class,'registerSubmit'] )->name('registerSubmit');
+Route::get('/addTags',[RegistrovaniKontrolerBogdan::class,'addTags'] )->name('addTags');
+Route::get('/removeTags',[RegistrovaniKontrolerBogdan::class,'removeTags'] )->name('removeTags');
+Route::get('/createAuctionVirtual',[RegistrovaniKontrolerBogdan::class,'createAuctionVirtual'] )->name('createAuctionVirtual');
+Route::post('/createAuctionSubmit',[RegistrovaniKontrolerBogdan::class,'createAuctionSubmit'] )->name('createAuctionSubmit');
+Route::get('/createAuctionPhysical',[RegistrovaniKontrolerBogdan::class,'createAuctionPhysical'] )->name('createAuctionPhysical');
+Route::post('/removeTags2',[RegistrovaniKontrolerBogdan::class,'removeTags2'] )->name('removeTags2');
+Route::post('/addTagsSubmit',[RegistrovaniKontrolerBogdan::class,'addTagsSubmit'] )->name('addTagsSubmit');
+Route::get('/upgradeUserRoles',[RegistrovaniKontrolerBogdan::class,'upgradeUserRoles'] )->name('upgradeUserRoles');
+Route::post('/upgradeUserRolesSubmit',[RegistrovaniKontrolerBogdan::class,'upgradeUserRolesSubmit'] )->name('upgradeUserRolesSubmit');
+Route::get('/logout',[RegistrovaniKontrolerBogdan::class,'logout'] )->name('logout');
+Route::get('/test2',[GostControllerBogdan::class,'test2'] )->name('test2');
+Route::get('/test3',[GostControllerBogdan::class,'test3'] )->name('test3');
+Route::get('/test4',[GostControllerBogdan::class,'test4'] )->name('test4');
 
 Route::get('/createExhibition', [CreateExhibitionController::class,'createExhibition'])->name('createExhibition');
-
 Route::get('/myExhibition', [CreateExhibitionController::class,'myExhibition']);
-
 Route::post('/createExhibitionSubmit', [CreateExhibitionController::class, 'createExhibitionSubmit'])->name('createExhibitionSubmit');
 
